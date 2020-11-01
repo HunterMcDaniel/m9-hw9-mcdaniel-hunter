5b16a9a03b5a6ea6db928b324c0fec37adea2aa2
“Hunter <hunterlmcdaniel@ufl.edu>
2020-11-01 15:26:47 -0500

    complete steps i - iii

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
-document.getElementsByClassName('Desserts');
-console.log('Desserts');
+class Desserts{
+  constructor (type, calories) {
+    this.type = type;
+    this.calories = calories;
+  }
+
+}
+
+class IceCream extends Desserts{
+  constructor (type, calories, flavor, scoops){
+    super(type, calories);
+    this.flavor = flavor;
+    this.scoops = scoops;
+  }
+}
